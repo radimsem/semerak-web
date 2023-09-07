c5b32b36e4daeb106b6c2b1982983b625fc6106c
radimsem <radim.semerak@gmail.com>
2023-09-07 22:15:27 +0200

    Adding Files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,20 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    backgroundSize: {
+      'heading-underline-size': '100% .6rem'
+    },
+    backgroundPosition: {
+      'heading-underline-pos': '0 85%'
+    },
+    screens: {
+      'xs': '390px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
